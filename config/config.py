c791e48df0567b107d3600882ae6efe330544859
wwwjjjnnn <1194348056@qq.com>
2019-09-17 15:40:21 +0800

    update HMM,BI-LSTM,CRF,BI-LSTM-CRF

---
+++ config/config.py
@@ -0,0 +1,14 @@
+# 设置lstm训练参数
+class TrainingConfig(object):
+    batch_size = 64
+    # 学习速率
+    lr = 0.001
+    epoches = 30
+    print_step = 5
+
+
+class LSTMConfig(object):
+    # 词向量的维数
+    emb_size = 128
+    # lstm隐向量的维数
+    hidden_size = 128
